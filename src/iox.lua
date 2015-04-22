ffcc98f4503a7ade76cc25235f0ade4fbb5ec454
Oscar Lim <olim@ucla.edu>
2015-04-22 15:42:34 -0700

    Add bi-directional popen
    This adds a bi-directional `popen` function called `popen2`, which can
    be used to read from and write to a spawned process.

--- src/iox.lua
+++ src/iox.lua
@@ -2,7 +2,20 @@ local ex_core = require 'ex.core'
 local iox = {
   lock   = ex_core.lock,
   unlock = ex_core.unlock,
+
   pipe   = ex_core.pipe,
+
+  popen2 = function(...)
+    local proc_rd, wr = ex_core.pipe()
+    local rd, proc_wr = ex_core.pipe()
+    local proc, err = ex_core.spawn{stdin = proc_rd, stdout = proc_wr, ...}
+    proc_rd:close(); proc_wr:close()
+    if not proc then
+      wr:close(); rd:close()
+      return proc, err
+    end
+    return proc, rd, wr
+  end
 }
 
 return iox
